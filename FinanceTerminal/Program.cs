dc1178a5e310b4e0fccd39556878f57f7984631f
Jack Benham <jack.benham34@gmail.com>
2021-07-04 13:40:28 +0100

    Added basic prompt handling

--- FinanceTerminal/Program.cs
+++ FinanceTerminal/Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using FinanceTerminal.Securities;
+
 
 namespace FinanceTerminal
 {
-    class Program
+    static class Program
     {
+        static ISecurity currentSecurity;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool running = true;
+
+            while (running)
+            {
+                PrintPrompt();
+                HandlePrompt();
+            }
+        }
+
+        private static void PrintPrompt()
+        {
+            Console.Write(currentSecurity.Symbol);
+            Console.Write('>');
+        }
+
+        private static void HandlePrompt()
+        {
+            string input = Console.ReadLine();
+            string[] tokens = input.Split(' ');
+
+            if (tokens.Length == 1)
+                // User is inputting a single security / command
+            {
+
+            }
+            else if (tokens.Length == 2)
+                // User is inputting a security and command
+            {
+
+            }
+            else
+                // User has malformed command
+            {
+
+            }
         }
     }
 }
